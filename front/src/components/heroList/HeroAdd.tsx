60c2251832ec0e26d145309e999b6180a6a8743a
4tiria <fedro_777@mail.ru>
2025-07-04 18:45:44 +0400

    .

--- front/src/components/heroList/HeroAdd.tsx
+++ front/src/components/heroList/HeroAdd.tsx
@@ -1,14 +1,14 @@
-﻿import React, {useState} from 'react';
-import {BsPlusLg} from "react-icons/all";
+﻿import React from 'react'
+import { BsPlusLg } from 'react-icons/all'
 
 const HeroAdd = () => {
-    const [isHovering, setIsHovering] = useState(false);
-    
     return (
         <div className="hero hero-default text-center d-flex justify-content-center align-items-center">
-           <div><BsPlusLg className="plus"/></div>
+            <div>
+                <BsPlusLg className="plus" />
+            </div>
         </div>
-    );
-};
+    )
+}
 
-export default HeroAdd;
+export default HeroAdd
