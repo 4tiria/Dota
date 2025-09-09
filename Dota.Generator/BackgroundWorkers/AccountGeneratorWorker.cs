69bd5dc6762a285f24165c14121af1b6826564ca
4tiria <fedro_777@mail.ru>
2025-09-09 20:41:52 +0400

    chore: add new background worker

---
+++ Dota.Generator/BackgroundWorkers/AccountGeneratorWorker.cs
@@ -0,0 +1,31 @@
+﻿using Dota.Generator.Model;
+
+namespace Dota.Generator.BackgroundWorkers;
+
+public class AccountGeneratorWorker(ILogger<AccountGeneratorWorker> logger) : BackgroundService
+{
+    protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+    {
+        while (!stoppingToken.IsCancellationRequested)
+        {
+            var line = await Task.Run(Console.ReadLine, stoppingToken);
+            if (line == null)
+            {
+                continue;
+            }
+
+            var account = new GenerateAccountRequest
+            {
+                Id = Guid.NewGuid(),
+                CreationDate = DateTime.UtcNow,
+                NickName = $"User_{Guid.NewGuid():N}".Substring(0, 8),
+                Email = null,
+                Avatar = null
+            };
+
+            // TODO: логика отправки на эндпоинт
+            
+            logger.LogInformation("Generated account {@Account}", account);
+        }
+    }
+}
---
+++ Dota.Generator/Model/GenerateAccountRequest.cs
@@ -0,0 +1,16 @@
+﻿using System.Reflection.Metadata;
+
+namespace Dota.Generator.Model;
+
+public class GenerateAccountRequest
+{
+    public Guid Id { get; set; }
+
+    public DateTime CreationDate { get; set; }
+
+    public string NickName { get; set; }
+    
+    public string? Email { get; set; }
+    
+    public Blob? Avatar { get; set; }
+}
