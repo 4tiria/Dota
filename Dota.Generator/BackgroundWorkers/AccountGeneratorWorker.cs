f2075a7a3c44ec312ede5693686cfc02ec86c888
4tiria <fedro_777@mail.ru>
2025-09-10 21:35:50 +0400

    chore: sending messages to kafka (accounts topic)

--- Dota.Generator/BackgroundWorkers/AccountGeneratorWorker.cs
+++ Dota.Generator/BackgroundWorkers/AccountGeneratorWorker.cs
@@ -1,11 +1,20 @@
-﻿using Dota.Generator.Model;
+﻿using System.Text.Json;
+using Confluent.Kafka;
+using Dota.Generator.Model;
 
 namespace Dota.Generator.BackgroundWorkers;
 
-public class AccountGeneratorWorker(ILogger<AccountGeneratorWorker> logger) : BackgroundService
+public class AccountGeneratorWorker(ILogger<AccountGeneratorWorker> logger, IConfiguration configuration) : BackgroundService
 {
+    private readonly ProducerConfig _config = new()
+    {
+        BootstrapServers = configuration["Kafka:Url"]
+    };
+    
     protected override async Task ExecuteAsync(CancellationToken stoppingToken)
     {
+        using var producer = new ProducerBuilder<string, string>(_config).Build();
+        
         while (!stoppingToken.IsCancellationRequested)
         {
             var line = await Task.Run(Console.ReadLine, stoppingToken);
@@ -18,12 +27,28 @@ public class AccountGeneratorWorker(ILogger<AccountGeneratorWorker> logger) : Ba
             {
                 Id = Guid.NewGuid(),
                 CreationDate = DateTime.UtcNow,
-                NickName = $"User_{Guid.NewGuid():N}".Substring(0, 8),
+                NickName = $"User_{Guid.NewGuid():N}"[..8],
                 Email = null,
                 Avatar = null
             };
+            
+            var message = new Message<string, string>
+            {
+                Key = account.Id.ToString(),
+                Value = JsonSerializer.Serialize(account)
+            };
 
-            // TODO: логика отправки на эндпоинт
+            try
+            {
+                var result = await producer.ProduceAsync("accounts", message, stoppingToken);
+                logger.LogInformation("Sent account {Id} to Kafka partition {Partition} offset {Offset}",
+                    account.Id, result.Partition, result.Offset);
+            }
+            catch (ProduceException<string, string> e)
+            {
+                logger.LogError(e, "Delivery failed: {Reason}", e.Error.Reason);
+                throw;
+            }
             
             logger.LogInformation("Generated account {@Account}", account);
         }
